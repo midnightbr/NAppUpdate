360d0fef216298cafbd4d6d16b34a4fc1178289e
midnightbr <marcosriderbr@outlook.com>
2023-05-05 15:20:56 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace NAppUpdate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            var titleAttribute = assembly.GetCustomAttributes(typeof(System.Reflection.AssemblyTitleAttribute), false).FirstOrDefault() as System.Reflection.AssemblyTitleAttribute;
+            string title = titleAttribute?.Title;
+            string version = assembly.GetName().Version.ToString();
+            Console.WriteLine(string.Format($"Assembly Title: {title}"));
+            Console.WriteLine(string.Format($"Assembly Version: {version}"));
+            Console.ReadLine();
+        }
+    }
+}
